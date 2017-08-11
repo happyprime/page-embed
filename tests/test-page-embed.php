afb97ca41a0c3b8b29f5e80f50e3c08cf52e7649
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-10 22:26:04 -0700

    Add initial sample tests from scaffold

---
+++ tests/test-page-embed.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class PageEmbedTest
+ *
+ * @package Page Embed
+ */
+
+/**
+ * Tests for the page_embed shortcode.
+ */
+class PageEmbedTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
