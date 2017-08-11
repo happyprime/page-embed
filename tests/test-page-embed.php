1cc24a9b5d1f6cfefd1962a8c87203e95ee8907c
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-10 22:32:45 -0700

    Add an initial content test for page_embed

--- tests/test-page-embed.php
+++ tests/test-page-embed.php
@@ -10,11 +10,20 @@
  */
 class PageEmbedTest extends WP_UnitTestCase {
 
-	/**
-	 * A single example test.
-	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+	function test_page_embed_with_no_wrapper_or_container() {
+		$embedded = wp_insert_post( array(
+			'post_type' => 'page',
+			'post_content' => 'This is my sample content.',
+		) );
+
+		$page = wp_insert_post( array(
+			'post_type' => 'page',
+			'post_content' => '[page_embed id=' . $embedded . ']',
+		) );
+
+		$page_content = apply_filters( 'the_content', get_post( $page )->post_content );
+		$expected_content = '<p>This is my sample content.</p>';
+
+		$this->assertEquals( $expected_content, $page_content );
 	}
 }
